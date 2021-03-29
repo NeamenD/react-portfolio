7b8fbd42ffd619377c752890a769ca23f3fd8ec8
Neamen_Dobamo <Neamenbirhanu@gmail.com>
2021-03-28 22:58:53 -0600

    added projectInfo

---
+++ client/src/data.js
@@ -0,0 +1,68 @@
+export default
+    {
+        
+        name :'Neamen Dobamo',
+        headerTagline: [
+                        'I am Neamen Dobamo.',
+                        'a full stack web developer',
+                        'from Denver'
+    ],
+        contactEmail:'neamenbirhanu@gmail.com',
+        resume: "./neamenResume.png",
+        abouttext: " my primary focus and inspiration for my studies is Web Development  I am very passionate about Web Development, and strive to better myself as a developer, and the development community as a whole. ",
+        aboutImage:'./profile.jpg',
+       ShowAboutImage:true,
+ 
+       projects:[
+        {
+            id: 1,
+            title: 'DeepVibrations',
+            service: 'Listion to your favorit music with DeepVibrations',
+            imageSrc: "./deepVibrations.png",
+            url: 'https://tevvels.github.io/Project_One_Deep_Vibrations/index.html'
+        },
+        {
+            id: 2,
+            title: 'Workday Schedule',
+            service: 'Schedule your day',
+            imageSrc: "./dayScheduler.JPG",
+            url: 'https://neamend.github.io/workdayschedule.github.io/.'
+        },
+
+           {
+            id: 3,
+            title:'Password Generator', 
+             service:'Simply generator a strong and reliable passWord', 
+             imageSrc:"./passWord.JPG",
+             url:'https://neamend.github.io/passwordHacksPloiter.github.io/.'
+            },
+            {
+                id: 6,
+                title: 'Quiz',
+                service: 'quiz to test your programming skill.',
+                imageSrc: "quizSc.jpg",
+                url: 'https://github.com/NeamenD/quiztimed.github.io'
+           },
+            { 
+                id: 7,
+                title: 'Inventory Management',
+                service: 'Asset management',
+                imageSrc: "IMSYSTEM.JPG",
+                url: 'https://nameless-sands-95928.herokuapp.com/'
+            },
+
+                        
+                
+               
+        ],
+        social: [
+
+            {   name:'Github',
+                url:'https://github.com/NeamenD'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/neamen-dobamo-828812199/'
+            },
+        ]
+    }     
+        
